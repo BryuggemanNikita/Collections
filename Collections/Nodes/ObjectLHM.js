579fbe1b5765fe44ae861301e06fb1d1151384c2
Nikita <briuggemanik@gmail.com>
2024-08-05 16:26:17 +0300

    _

--- Collections/Nodes/ObjectLHM.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toString = toString;
-function toString(obj) {
-    return "".concat(obj.key).concat(obj.value, "obj");
-}
